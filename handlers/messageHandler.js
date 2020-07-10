6f2ee2e14f5928398542999fb95c9714cc9c0f0f
Alex Sun <alexsun82@icloud.com>
2020-07-10 18:26:18 +0200

    Extract message handler

---
+++ handlers/messageHandler.js
@@ -0,0 +1,32 @@
+import rollDice from "../modules/rollDice.js";
+
+// Set command prefix for the bot.
+const PREFIX = "!";
+
+const messageHandler = msg => {
+    if (msg.content.substring(0, PREFIX.length) !== PREFIX) {
+        return void 0;
+    } else {
+        const argv = msg.content.substring(PREFIX.length).split(" ");
+        switch (argv[0]) {
+            case "ping":
+                msg.channel.send("Pong!");
+                break;
+            case "version":
+                msg.channel.send(
+                    `I am currently running version ${process.env.npm_package_version}.`
+                );
+                break;
+            case "roll":
+            case "r":
+                msg.channel.send(
+                    rollDice(argv[1], argv[2] === "--stats" || argv[2] === "-s")
+                );
+                break;
+            default:
+                return void 0;
+        }
+    }
+};
+
+export default messageHandler;
--- index.js
+++ index.js
@@ -1,41 +1,13 @@
 import Discord from "discord.js";
-
-import rollDice from "./modules/rollDice.js";
+import messageHandler from "./handlers/messageHandler.js";
 
 // Create the bot instance.
 const bot = new Discord.Client();
-
-// Set command prefix for the bot.
-const PREFIX = "!";
 
 bot.once("ready", () => {
     console.log("Up and running!");
 });
 
-bot.on("message", msg => {
-    if (msg.content.substring(0, PREFIX.length) !== PREFIX) {
-        return void 0;
-    } else {
-        const argv = msg.content.substring(PREFIX.length).split(" ");
-        switch (argv[0]) {
-            case "ping":
-                msg.channel.send("Pong!");
-                break;
-            case "version":
-                msg.channel.send(
-                    `I am currently running version ${process.env.npm_package_version}.`
-                );
-                break;
-            case "roll":
-            case "r":
-                msg.channel.send(
-                    rollDice(argv[1], argv[2] === "--stats" || argv[2] === "-s")
-                );
-                break;
-            default:
-                return void 0;
-        }
-    }
-});
+bot.on("message", messageHandler);
 
 bot.login(process.env.DISCORD_TOKEN);
