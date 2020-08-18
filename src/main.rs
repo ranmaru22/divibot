9982209b1577b7f39ef5cafedb9ef59672d0fb72
Alex Sun <alexsun82@icloud.com>
2020-08-18 15:03:31 +0200

    Add framework and some debug code for testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use serenity::{
     async_trait,
+    framework::standard::{
+        macros::{command, group},
+        CommandResult, StandardFramework,
+    },
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
@@ -11,25 +15,42 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why)
-            }
-        }
-    }
-
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} connected", ready.user.name);
     }
 }
 
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.channel_id.say(&ctx.http, "pong!").await {
+        println!("error sending message: {:?}", why)
+    }
+    Ok(())
+}
+
+#[command]
+async fn foo(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.channel_id.say(&ctx.http, "bar").await {
+        println!("error sending message: {:?}", why)
+    }
+    Ok(())
+}
+
+#[group]
+#[commands(ping, foo)]
+struct Public;
+
 #[tokio::main]
 async fn main() {
     let config = Config::new();
 
+    let framework = StandardFramework::new()
+        .configure(|cfg| cfg.with_whitespace(true).prefix(config.prefix()))
+        .group(&PUBLIC_GROUP);
+
     let mut client = Client::new(config.token())
         .event_handler(Handler)
+        .framework(framework)
         .await
         .expect("Error creating client");
 
