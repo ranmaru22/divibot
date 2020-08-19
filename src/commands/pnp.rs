f0e3fcfb6ccd682c0ecfb44a1bf324abc713a811
Alex Sun <alexsun82@icloud.com>
2020-08-19 17:48:08 +0200

    Implement arg parsing

--- src/commands/pnp.rs
+++ src/commands/pnp.rs
@@ -15,7 +15,27 @@ async fn roll(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         .split(dice::is_delimiter)
         .map(|c| c.parse().unwrap_or_default())
         .collect();
-    let opts = RollOptions::Nothing;
+
+    let mut opts = Vec::new();
+    while !args.is_empty() {
+        if let Ok(opt) = args.single::<String>() {
+            match opt {
+                opt if opt.contains("-e") => {
+                    let arg = arg_parse(&opt, "-e");
+                    opts.push(RollOptions::ExplodeOn(arg));
+                }
+                opt if opt.contains("-c") => {
+                    let arg = arg_parse(&opt, "-c");
+                    opts.push(RollOptions::CountSuccesses(arg));
+                }
+                _ => continue,
+            }
+        }
+    }
+    if opts.is_empty() {
+        opts.push(RollOptions::Nothing);
+    }
+
     let random_rolls = match dice::roll_dice(dice_args, opts) {
         Some(result) => result,
         None => return Ok(()),
@@ -26,4 +46,12 @@ async fn roll(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     }
 
     Ok(())
+}
+
+fn arg_parse(arg: &str, split_on: &str) -> u32 {
+    *arg.split(split_on)
+        .map(|c| c.parse().unwrap_or_default())
+        .collect::<Vec<u32>>()
+        .last()
+        .unwrap_or(&0)
 }
