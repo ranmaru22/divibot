f52642e71578a7b2591cc10f683a5a6fbcbc646b
Alex Sun <alexsun82@icloud.com>
2020-08-21 11:12:53 +0200

    Implement reroll functionality

--- src/commands/pnp/dice.rs
+++ src/commands/pnp/dice.rs
@@ -9,14 +9,23 @@ mod tests {
     #[test]
     fn dice_roll_works() {
         let (sides, num_dice) = (6, 100);
-        let results = roll(sides, num_dice);
+        let results = roll(sides, num_dice, None);
         assert!(results.iter().all(|&x| x <= sides));
         assert!(results.len() == num_dice as usize);
     }
+
+    #[test]
+    fn reroll_works() {
+        let (sides, num_dice, reroll) = (6, 100, 3);
+        let results = roll(sides, num_dice, Some(reroll));
+        assert!(results.iter().all(|&x| x != reroll));
+        assert!(results.len() == num_dice as usize);
+    }
+
     #[test]
     fn exploding_roll_works() {
         let (sides, num_dice, explode_on) = (6, 100, 6);
-        let results = exploding_roll(sides, num_dice, explode_on);
+        let results = exploding_roll(sides, num_dice, explode_on, None);
         assert!(results.iter().all(|&x| x % 6 != 0));
         assert!(results.len() == num_dice as usize);
     }
@@ -54,30 +63,45 @@ pub fn roll_dice(args: Vec<u32>, mut opts: Vec<RollOptions>) -> Option<String> {
         return None;
     }
 
-    let mut results = roll(sides, num_dice);
+    let mut reroll: Option<u32> = None;
+    let mut results = roll(sides, num_dice, None);
     if !opts.is_empty() {
         opts.sort();
         for opt in opts.iter() {
             match opt {
-                RollOptions::Nothing => break,
-                RollOptions::ExplodeOn(val) => results = exploding_roll(sides, num_dice, *val),
+                RollOptions::RerollOn(val) => reroll = Some(*val),
+                RollOptions::Nothing => match reroll {
+                    None => continue,
+                    Some(_) => results = roll(sides, num_dice, reroll),
+                },
+                RollOptions::ExplodeOn(val) => {
+                    results = exploding_roll(sides, num_dice, *val, reroll)
+                }
                 RollOptions::CountSuccesses(val) => results.set_succ_threshold(*val),
                 RollOptions::KeepBest(val) => results.keep_best(*val),
                 RollOptions::DropLowest(val) => results.drop_lowest(*val),
             }
         }
     }
-
     let result_str = format!("{}", results);
     Some(result_str)
 }
 
 /// Rolls a number of dice and returns the results.
-fn roll(sides: u32, num_dice: u32) -> DiceResults {
+fn roll(sides: u32, num_dice: u32, reroll: Option<u32>) -> DiceResults {
     let mut rng = thread_rng();
     let mut rolls = DiceResults::new(num_dice as usize);
     for _ in 0..num_dice {
-        let n: u32 = rng.gen_range(1, sides + 1);
+        let n: u32 = match reroll {
+            None => rng.gen_range(1, sides + 1),
+            Some(val) => {
+                let mut candidate = val;
+                while candidate == val {
+                    candidate = rng.gen_range(1, sides + 1);
+                }
+                candidate
+            }
+        };
         rolls.push(n);
     }
     rolls
@@ -85,18 +109,25 @@ fn roll(sides: u32, num_dice: u32) -> DiceResults {
 
 /// Rolls a number of dice, rolls again when the roll matches the explode_on
 /// argument, and restuns the results.,
-fn exploding_roll(sides: u32, num_dice: u32, explode_on: u32) -> DiceResults {
+fn exploding_roll(sides: u32, num_dice: u32, explode_on: u32, reroll: Option<u32>) -> DiceResults {
     let mut rng = thread_rng();
     let mut rolls = DiceResults::new(num_dice as usize);
     for _ in 0..num_dice {
         let mut result: u32 = 0;
-        let mut n;
-        loop {
+        let mut n = match reroll {
+            None => rng.gen_range(1, sides + 1),
+            Some(val) => {
+                let mut candidate = val;
+                while candidate == val {
+                    candidate = rng.gen_range(1, sides + 1);
+                }
+                candidate
+            }
+        };
+        result += n;
+        while n == explode_on {
             n = rng.gen_range(1, sides + 1);
             result += n;
-            if n != explode_on {
-                break;
-            };
         }
         rolls.push(result);
     }
